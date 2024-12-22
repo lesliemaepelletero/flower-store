9f080b5ca84da1337393adb9f72ab9b1fe36558a
unknown <vtiu635@gmail.com>
2024-12-22 14:25:17 +0800

    fix/feat: implement the table valued function ba to

--- admin_contacts.php
+++ admin_contacts.php
@@ -7,13 +7,14 @@ session_start();
 $admin_id = $_SESSION['admin_id'];
 
 if (!isset($admin_id)) {
-   header('location:login.php');
-};
+    header('location:login.php');
+}
 
+// Deleting a message
 if (isset($_GET['delete'])) {
-   $delete_id = $_GET['delete'];
-   pg_query($conn, "DELETE FROM message WHERE id = '$delete_id'") or die('query failed');
-   header('location:admin_contacts.php');
+    $delete_id = $_GET['delete'];
+    pg_query($conn, "DELETE FROM message WHERE id = '$delete_id'") or die('Query failed');
+    header('location:admin_contacts.php');
 }
 
 ?>
@@ -22,54 +23,55 @@ if (isset($_GET['delete'])) {
 <html lang="en">
 
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>dashboard</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Messages</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom admin css file link  -->
-   <link rel="stylesheet" href="css/admin_style.css">
-
+    <!-- custom admin css file link  -->
+    <link rel="stylesheet" href="css/admin_style.css">
 </head>
 
 <body>
 
-   <?php @include 'admin_header.php'; ?>
+    <?php @include 'admin_header.php'; ?>
 
-   <section class="messages">
+    <section class="messages">
 
-      <h1 class="title">messages</h1>
+        <h1 class="title">Messages</h1>
 
-      <div class="box-container">
+        <div class="box-container">
+            <?php
+            // Query all messages using the table-valued function
+            $query = "SELECT * FROM message";
+            $select_message = pg_query($conn, $query) or die('Query failed');
 
-         <?php
-         $select_message = pg_query($conn, "SELECT * FROM message") or die('query failed');
-         if (pg_num_rows($select_message) > 0) {
-            while ($fetch_message = pg_fetch_assoc($select_message)) {
-         ?>
-               <div class="box">
-                  <p>user id : <span><?php echo $fetch_message['user_id']; ?></span> </p>
-                  <p>firstname : <span><?php echo $fetch_message['first_name']; ?></span> </p>
-                  <p>lastname : <span><?php echo $fetch_message['last_name']; ?></span> </p>
-                  <p>number : <span><?php echo $fetch_message['phone_number']; ?></span> </p>
-                  <p>email : <span><?php echo $fetch_message['email']; ?></span> </p>
-                  <p>message : <span><?php echo $fetch_message['message']; ?></span> </p>
-                  <a href="admin_contacts.php?delete=<?php echo $fetch_message['id']; ?>" onclick="return confirm('delete this message?');" class="delete-btn">delete</a>
-               </div>
-         <?php
+            if (pg_num_rows($select_message) > 0) {
+                while ($fetch_message = pg_fetch_assoc($select_message)) {
+            ?>
+                    <div class="box">
+                        <p>User ID: <span><?php echo $fetch_message['user_id']; ?></span></p>
+                        <p>First Name: <span><?php echo $fetch_message['first_name']; ?></span></p>
+                        <p>Last Name: <span><?php echo $fetch_message['last_name']; ?></span></p>
+                        <p>Phone Number: <span><?php echo $fetch_message['phone_number']; ?></span></p>
+                        <p>Email: <span><?php echo $fetch_message['email']; ?></span></p>
+                        <p>Message: <span><?php echo $fetch_message['message']; ?></span></p>
+                        <a href="admin_contacts.php?delete=<?php echo $fetch_message['id']; ?>" onclick="return confirm('Delete this message?');" class="delete-btn">Delete</a>
+                    </div>
+            <?php
+                }
+            } else {
+                echo '<p class="empty">You have no messages!</p>';
             }
-         } else {
-            echo '<p class="empty">you have no messages!</p>';
-         }
-         ?>
-      </div>
+            ?>
+        </div>
 
-   </section>
+    </section>
 
-   <script src="js/admin_script.js"></script>
+    <script src="js/admin_script.js"></script>
 
 </body>
 
